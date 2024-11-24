9ac12759e6cadd635bab43e109f8b853dd475385
Paulaxx <palinkagr@gmail.com>
2024-11-24 15:56:48 +0100

    initial commir

---
+++ src/main/java/com/example/footballworldcupscoreboardapp/FootballWorldCupScoreBoardAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.footballworldcupscoreboardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballWorldCupScoreBoardAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballWorldCupScoreBoardAppApplication.class, args);
+    }
+
+}
